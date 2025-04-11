7771d94f5d2f3d66667faada2194c3265cef9695
GOchoa2111 <gochoa.2111@gmail.com>
2025-04-11 17:20:25 -0600

    Primera carga de proyecto

---
+++ Backend/ApiVentas/ApiVentas/Context/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using ApiVentas.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiVentas.Context
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
